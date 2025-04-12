e5f761b7c24e1777def9bc98c25831c21a877fce
LeoLiao031 <114187109+LeoLiao031@users.noreply.github.com>
2025-04-11 23:51:16 -0400

    numbered and fixed table creation ofr Records

--- express-server/db_population/records_table_setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Records (
-    port_id INT NOT NULL REFERENCES Ports(port_id) ON DELETE CASCADE,
-    record_type TEXT NOT NULL,
-    amount NUMERIC(12,2) NOT NULL,
-    date DATE NOT NULL,
-)
